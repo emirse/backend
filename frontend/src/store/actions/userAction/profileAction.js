c5bfd4a8c2b4de54e7d7bf33877f70c7e258a996
Emir <erdemires@hotmail.com>
2023-01-16 04:32:35 +0300

    'fix'

--- frontend/src/store/actions/userAction/profileAction.js
+++ frontend/src/store/actions/userAction/profileAction.js
@@ -2,32 +2,6 @@ import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 const backendUrl = "http://localhost:8000";
-<<<<<<< HEAD
-export const userProfile = createAsyncThunk("user/profile", async () => {
-  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-
-  try {
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${userInfo.token}`,
-      },
-    };
-    const { data } = await axios.get(
-      `${backendUrl}/api/user/profile/${userInfo.id}`,
-      config
-    );
-    console.log("profile", data);
-    return data;
-  } catch (error) {
-    if (error.response && error.response.data.message) {
-      return error.response.data.message;
-    } else {
-      return error.message;
-    }
-  }
-});
-=======
 
 export const userProfile = createAsyncThunk(
   "user/profile",
@@ -51,4 +25,3 @@ export const userProfile = createAsyncThunk(
     }
   }
 );
->>>>>>> df0ee14 (fix)
